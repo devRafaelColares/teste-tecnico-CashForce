b02b99a07e7089f60106b0e5cf5b739d12764d74
Rafael Colares <rafaelcolares.dev@gmail.com>
2024-08-05 10:23:29 -0300

    Desenvolvendo o frontend.

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="header"></header>
+    <div  class="headerTable">
+      <img src="../assets/Frame753.png" alt="">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HeaderComponent"
+  };
+  </script>
+  
+  <style scoped>
+  .header {
+    width: 1272px;
+    height: 40px;
+    border-bottom: 1px solid #DFE2EB;
+  }
+  
+  .headerTable {
+    margin-top: 20px;
+  }
+  </style>
+  
